d3edc91d27e6522a50fc329916c64c94e7fca81f
wangxiao <wangxiao@qdcares.cn>
2020-08-25 23:39:55 +0800

    feat: 添加生命周期

--- toy-react/toy-react.js
+++ toy-react/toy-react.js
@@ -1,20 +1,46 @@
+const RENDER_TO_DOM = Symbol('render to dom');
+
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type);
   }
 
   setAttribute(name, value) {
-    this.root.setAttribute(name, value);
+    if (name.match(/^on([\s\S]+)/)) {
+      this.root.addEventListener(
+        RegExp.$1.replace(/[\s\S]/, c => c.toLowerCase()),
+        value
+      );
+    } else {
+      if (name === 'className') {
+        name = 'class';
+      }
+      this.root.setAttribute(name, value);
+    }
   }
 
   appendChild(component) {
-    this.root.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
+  }
+
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content);
+  }
+
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -24,6 +50,7 @@ export class Component {
     this.children = [];
 
     this._root = null;
+    this._range = null;
   }
 
   setAttribute(name, value) {
@@ -34,12 +61,42 @@ export class Component {
     this.children.push(component);
   }
 
-  get root() {
-    if (!this._root) {
-      this._root = this.render().root;
-    }
+  [RENDER_TO_DOM](range) {
+    this._range = range;
+    this.render()[RENDER_TO_DOM](range);
+  }
 
-    return this._root;
+  rerender() {
+    const oldRange = this._range;
+    const range = document.createRange();
+    range.setStart(oldRange.startContainer, oldRange.startOffset);
+    range.setEnd(oldRange.startContainer, oldRange.startOffset);
+
+    this[RENDER_TO_DOM](range);
+
+    oldRange.setStart(range.endContainer, range.endOffset);
+    oldRange.deleteContents();
+  }
+
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState;
+      this.rerender();
+      return;
+    }
+    const merge = (oldState, newState) => {
+      for (const key in newState) {
+        if (oldState[key] === null || typeof oldState[key] !== 'object') {
+          oldState[key] = newState[key];
+        } else {
+          merge(oldState[key], newState[key]);
+        }
+      }
+    };
+
+    merge(this.state, newState);
+
+    this.rerender();
   }
 }
 
@@ -70,6 +127,10 @@ export function createElement(type, attributes, ...children) {
         child = new TextWrapper(child);
       }
 
+      if (child === null) {
+        continue;
+      }
+
       if (Array.isArray(child)) {
         insertChildren(child);
       } else {
@@ -84,5 +145,10 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-  parentElement.appendChild(component.root);
+  let range = document.createRange();
+  range.setStart(parentElement, 0);
+  range.setEnd(parentElement, parentElement.childNodes.length);
+  range.deleteContents();
+
+  component[RENDER_TO_DOM](range);
 }
